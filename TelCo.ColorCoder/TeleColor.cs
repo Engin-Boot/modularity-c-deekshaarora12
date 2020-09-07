b6dde9e64724c271ba538ed0c4eebd7da1f50bb6
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-07 17:32:29 +0530

    Update and rename Program.cs to TeleColor.cs

--- TelCo.ColorCoder/TeleColor.cs
+++ TelCo.ColorCoder/TeleColor.cs
@@ -11,7 +11,7 @@ namespace TelCo.ColorCoder
     /// This class provides the color coding and 
     /// mapping of pair number to color and color to pair number.
     /// </summary>
-    public class Program
+    public class TeleColor
     {
         /// <summary>
         /// Array of Major colors
@@ -36,7 +36,7 @@ namespace TelCo.ColorCoder
         /// <summary>
         /// Static constructor required to initialize static variable
         /// </summary>
-        static Program()
+        static TeleColor()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
