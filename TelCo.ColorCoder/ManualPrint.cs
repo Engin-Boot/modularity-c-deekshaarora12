7a25b5421fce092855b31f39c4f2cd68bb775f33
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-08 10:35:34 +0530

    Create ManualPrint.cs

---
+++ TelCo.ColorCoder/ManualPrint.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Collections.Generic; 
+
+namespace TelCo.ColorCoder
+{
+public class ManualPrint: PairToColor
+{
+internal static int GetManual()
+        {
+            
+        }
+     }
+  }
