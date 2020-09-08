77058917138cc7fe349231d68801d7af0895fe70
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-08 12:17:46 +0530

    Update ManualPrint.cs

--- TelCo.ColorCoder/ManualPrint.cs
+++ TelCo.ColorCoder/ManualPrint.cs
@@ -10,7 +10,29 @@ public class ManualPrint: PairToColor
 {
 internal static int GetManual()
         {
+            string finalManual = "";
             
+            int pairNumber = 1;
+            
+            for(int i = 0; i < colorMapMajor.Length; i++)
+             {
+             
+             string printManual = "";
+             
+            for(int j = 0; j < colorMapMinor.Length; j++)
+             {
+              string majCol= colorMapMajor[i];
+              string minCol = colorMapMinor[j];
+              
+              string pairNumbertoString = pairNumber.ToString();
+              
+              printManual = printManual + pairNumbertoString majCol + minCol;
+              
+              pairNumber = pairNumber + 1;
+            }
+            finalManual = finalManual + printManual;
+          }
+          Console.writeLine(finalManual);
         }
      }
   }
